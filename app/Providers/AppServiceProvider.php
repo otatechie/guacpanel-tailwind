39b4515e26dfb0c6cb4752297df466329888f74f
Debugified <de.rooij.peter@gmail.com>
2025-06-15 21:43:18 +0200

    Fixes: https://github.com/otatechie/guacpanel-tailwind/issues/8 https://github.com/otatechie/guacpanel-tailwind/issues/2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Personalisation;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Storage;
@@ -20,24 +21,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Get personalization data
-        $personalisation = Personalisation::first() ?? new Personalisation();
-        if ($personalisation->favicon && !Storage::disk('public')->exists($personalisation->favicon)) {
-            $personalisation->favicon = null;
+        if (Schema::hasTable((new Personalisation())->getTable())) {
+            // Get personalization data
+            $personalisation = Personalisation::first() ?? new Personalisation();
+            if ($personalisation->favicon && !Storage::disk('public')->exists($personalisation->favicon)) {
+                $personalisation->favicon = null;
+            }
+
+            // Share with Laravel views
+            View::composer('*', function ($view) use ($personalisation) {
+                $view->with('personalisation', $personalisation);
+            });
+
+            // Share with Inertia
+            Inertia::share([
+                'app' => [
+                    'version' => config('app.version'),
+                    'name' => config('app.name'),
+                ],
+                'personalisation' => fn () => $personalisation
+            ]);
         }
-
-        // Share with Laravel views
-        View::composer('*', function ($view) use ($personalisation) {
-            $view->with('personalisation', $personalisation);
-        });
-
-        // Share with Inertia
-        Inertia::share([
-            'app' => [
-                'version' => config('app.version'),
-                'name' => config('app.name'),
-            ],
-            'personalisation' => fn () => $personalisation
-        ]);
     }
 }
