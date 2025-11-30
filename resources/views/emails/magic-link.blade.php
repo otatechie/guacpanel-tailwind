f54ed0f7d9fa63efd8cae957fbe3243464f6ea76
Jeremy Kenedy <jeremykenedy@gmail.com>
2025-11-30 05:42:28 -0800

    update prettierrc and run format

--- resources/views/emails/magic-link.blade.php
+++ resources/views/emails/magic-link.blade.php
@@ -1,18 +1,19 @@
 @component('mail::message')
-# {{ $isNewUser ? 'Complete Your Registration' : 'Login to Your Account' }}
+    # {{ $isNewUser ? 'Complete Your Registration' : 'Login to Your Account' }}
 
-@if($isNewUser)
-Welcome! Click the button below to verify your email and access your account.
-@else
-Click the button below to login to your account. This link will expire in 10 minutes.
-@endif
+    @if ($isNewUser)
+        Welcome! Click the button below to verify your email and access your account.
+    @else
+            Click the button below to login to your account. This link will expire in 10 minutes.
+    @endif
 
-@component('mail::button', ['url' => $url])
-{{ $isNewUser ? 'Access Your Account' : 'Login Now' }}
-@endcomponent
+    @component('mail::button', ['url' => $url])
+        {{ $isNewUser ? 'Access Your Account' : 'Login Now' }}
+    @endcomponent
 
-If you did not request this link, no action is required.
+    If you did not request this link, no action is required. Thanks,
 
-Thanks,<br>
-{{ config('app.name') }}
+    <br />
+
+    {{ config('app.name') }}
 @endcomponent
