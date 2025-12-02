be4b0f8f998a7b115c6ca9e1851f9dabc5959f64
Jeremy Kenedy <jeremykenedy@gmail.com>
2025-12-02 06:41:27 -0800

    lint

--- resources/views/emails/goodbye-user-mail.blade.php
+++ resources/views/emails/goodbye-user-mail.blade.php
@@ -1,8 +1,10 @@
 <x-mail::message>
-    # @lang('emails.goodbye.greeting', ['username' => $user->name])
+    #
+    @lang('emails.goodbye.greeting', ['username' => $user->name])
 
     @lang('emails.goodbye.message')
 
-    ## @lang('emails.goodbye.goodbye')
+    ##
+    @lang('emails.goodbye.goodbye')
     {{ config('app.name') }}
 </x-mail::message>
